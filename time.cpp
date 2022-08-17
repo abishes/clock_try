a6ab3a4ee7e8e57f9c095f288c093c8c437aec0f
obsessed <abishesshr@gmail.com>
2022-08-17 14:50:39 +0545

    Added clear

--- time.cpp
+++ time.cpp
@@ -5,14 +5,14 @@
 #define PI 3.14159265
 #define Wheight 750
 #define radius 300
-enum clockHand{H=15,M=10, S=5};
+enum clockHand{H=15,M=10, S=3};
 class Proto{
 public:
     Proto(){
         int length=260;
         float angle=PI/6;
         char numbers[13][3]={"3","4","5","6","7","8","9","10","11","12","1","2"};
-        setcolor(12);
+        setcolor(LIGHTRED);
         setlinestyle(SOLID_LINE,0,10);//The linestyle parameter does not affect circles. Only the thickness parameter is used.
         circle(Wheight/2,Wheight/2,radius);
         settextstyle(SANS_SERIF_FONT,0,3);
@@ -20,6 +20,11 @@ public:
         outtextxy(Wheight/2+int(cos(i*angle)*length), Wheight/2+int(sin(i*angle)*length),numbers[i]);
         plt(Wheight/2+int(cos(i*angle)*length), Wheight/2+int(sin(i*angle)*length));
         }
+        setcolor(LIGHTRED);
+        setlinestyle(SOLID_LINE,0,2);
+        circle(Wheight/2,Wheight/2,200);
+        setfillstyle(SOLID_FILL,BLACK);
+        floodfill(Wheight/2,Wheight/2,LIGHTRED);
     }
     void plt(int x,int y){
         for(int i=-1;i<=1;i++){
@@ -35,19 +40,56 @@ public:
     }
 };
 class hand{
-float thickness, length;
+int thickness, length;
+float angle;
 public:
-    hand(){
-        length=200;
+    hand(int x){
+        if(x==S){
+            length=200;
+            thickness=S;
+        }
+        else if(x==M){
+            length=180;
+            thickness=M;
+        }
+        else{
+            length=100;
+            thickness=H;
+        }
         setcolor(15);
-        setlinestyle(SOLID_LINE,0,S);
-        line(Wheight/2,Wheight/2,Wheight/2+150,Wheight/2+150);
+    }
+    void drawHand(float angle){
+        this->angle=angle;
+        setlinestyle(SOLID_LINE,0,thickness);
+        line(Wheight/2, Wheight/2, Wheight/2+int(cos(angle)*length), Wheight/2+int(sin(angle)*length));
+    }
+    ~hand(){
+        setcolor(BLACK);
+        setlinestyle(SOLID_LINE,0,thickness);
+        line(Wheight/2, Wheight/2, Wheight/2+int(cos(angle)*length), Wheight/2+int(sin(angle)*length));
     }
 };
+void drawArms(){
+    hand hour(H), minute(M), second(S);
+    hour.drawHand(0.3f);
+    minute.drawHand(1.2f);
+    second.drawHand(2.0f);
+    setcolor(LIGHTGRAY);
+    circle(Wheight/2,Wheight/2,12);
+    setfillstyle(1,LIGHTGRAY);
+    floodfill(Wheight/2,Wheight/2,LIGHTGRAY);
+    delay(2000);
+    //to clear the screen
+    setcolor(LIGHTRED);
+    setlinestyle(SOLID_LINE,0,2);
+    circle(Wheight/2,Wheight/2,200);
+    setfillstyle(SOLID_FILL,BLACK);
+    floodfill(Wheight/2,Wheight/2,LIGHTRED);
+}
 int main(){
-    initwindow(Wheight, Wheight,(char*)"Clock");
+    initwindow(Wheight, Wheight,"Clock");
     Proto ClockProto;
-    hand hrs;
+    drawArms();
     getch();
     closegraph();
 
