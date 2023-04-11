de4ea110562e8adc981900f85b2ad51e0238f286
obsessed <abishesshr@gmail.com>
2023-04-11 17:46:29 +0545

    fixed the hands to rotate at 12

--- time.cpp
+++ time.cpp
@@ -4,7 +4,7 @@
 #define PI 3.14159265
 #define Wheight 750
 #define radius 300
-enum clockHand{H=15,M=10, S=3};
+enum clockHand{H=14,M=8, S=4};
 class Proto{
 public:
     Proto(){
@@ -21,7 +21,7 @@ public:
         }
         setcolor(LIGHTRED);
         setlinestyle(SOLID_LINE,0,2);
-        circle(Wheight/2,Wheight/2,210);
+        circle(Wheight/2,Wheight/2,220);
     }
 
     void plt(int x,int y){
@@ -49,22 +49,12 @@ int thickness, length;
 float angle;
 public:
     hand(int x){
-        if(x==S){
-            length=200;
-            thickness=S;
-        }
-        else if(x==M){
-            length=180;
-            thickness=M;
-        }
-        else{
-            length=100;
-            thickness=H;
-        }
-        setcolor(15);
+        thickness = x;
+        length = 240 - (x*10);
     }
     void drawHand(float angle){
         this->angle=angle;
+        setcolor(15);
         setlinestyle(SOLID_LINE,0,thickness);
         line(Wheight/2, Wheight/2, Wheight/2+int(cos(angle)*length), Wheight/2+int(sin(angle)*length));
     }
@@ -79,16 +69,16 @@ void drawArms(float& angle_second, float& angle_minute, float& angle_hour){
     circle(Wheight/2,Wheight/2,12);
     setfillstyle(1,LIGHTGRAY);
     floodfill(Wheight/2,Wheight/2,LIGHTGRAY);
-    delay(100);
+    Sleep(100);
     //to clear the screen
     setfillstyle(SOLID_FILL,BLACK);
     floodfill(Wheight/2,Wheight/2,LIGHTRED);
     angle_second+=.11*(PI/30);
-    if(angle_second >= 2*PI){
+    if(angle_second >= 3*PI/2){
         angle_second -=2*PI;
         angle_minute +=PI/30;
     }
-    if(angle_minute >= 2*PI){
+    if(angle_minute >= 3*PI/2){
         angle_minute -=2*PI;
         angle_hour +=PI/30;
     }
@@ -100,14 +90,13 @@ int main(){
     time(&now);
     struct tm *mytime;
     initwindow(Wheight, Wheight, "Clock");
-    Proto ClockProto;
+    Proto ClockProto;    
     mytime = localtime(&now);
     float angle_hour=((mytime->tm_hour)%12)*(PI/6) + (mytime->tm_min)*(PI/360) + 3*(PI/2);
     float angle_minute=(mytime->tm_min)*(PI/30)+3*(PI/2);
     float angle_second= (mytime->tm_sec)*(PI/30)+3*(PI/2);
-    while(1){
+    while(1)
         drawArms(angle_second,angle_minute, angle_hour);
-    }
     closegraph();
     return 0;
 }
